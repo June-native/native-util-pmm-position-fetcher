c27fdb923928e42be7df731dc461ae2ee86a4738
June <june@native.org>
2025-11-06 13:07:59 +0800

    fix: vercel build

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
+  root: resolve(__dirname),
   build: {
     outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
+      input: resolve(__dirname, 'index.html'),
       output: {
         manualChunks: {
           ethers: ['ethers']
